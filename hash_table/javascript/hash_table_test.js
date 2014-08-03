0c0eeb5da8a7320e56b90ec558b66aadbb6e1d71
peferron <pe.ferron@gmail.com>
2014-08-03 12:39:39 +0800

    [hash_table/javascript] Add tests

--- hash_table/javascript/hash_table_test.js
+++ hash_table/javascript/hash_table_test.js
@@ -4,27 +4,91 @@ var assert = require('assert');
 
 var hash_table = require('./hash_table.js');
 
-var h = new hash_table.HashTable(10);
+function basicTests() {
+    var h = new hash_table.HashTable(10);
 
-assert(!h.has('abc'));
+    assert(!h.has('abc'));
 
-h.set('abc', 5);
-assert(h.has('abc'));
-assert.strictEqual(h.get('abc'), 5);
+    h.set('abc', 5);
+    assert(h.has('abc'));
+    assert.strictEqual(h.get('abc'), 5);
 
-h.set('abc', 7);
-assert(h.has('abc'));
-assert.strictEqual(h.get('abc'), 7);
+    h.set('abc', 7);
+    assert(h.has('abc'));
+    assert.strictEqual(h.get('abc'), 7);
 
-h.set('def', 9);
-assert(h.has('abc'));
-assert.strictEqual(h.get('abc'), 7);
-assert(h.has('def'));
-assert.strictEqual(h.get('def'), 9);
+    h.set('def', 9);
+    assert(h.has('abc'));
+    assert.strictEqual(h.get('abc'), 7);
+    assert(h.has('def'));
+    assert.strictEqual(h.get('def'), 9);
 
-h.del('abc');
-assert(!h.has('abc'));
-assert(h.has('def'));
-assert.strictEqual(h.get('def'), 9);
+    h.del('abc');
+    assert(!h.has('abc'));
+    assert(h.has('def'));
+    assert.strictEqual(h.get('def'), 9);
+}
+
+function randomTests() {
+    for (var i = 0; i < 100; i++) {
+        randomTest();
+    }
+}
+
+function randomTest() {
+    var h = new hash_table.HashTable(1000);
+    var m = {};
+    var a = [];
+
+    for (var i = 0; i < 10000; i++) {
+        var r = Math.random();
+        if (r < 0.2) {
+            delRandom(h, m, a);
+            continue;
+        }
+        setRandom(h, m, a);
+    }
+
+    check(h, m, a);
+}
+
+function check(h, m, a) {
+    a.forEach(function(k) {
+        assert(h.has(k));
+        assert.strictEqual(m[k], h.get(k));
+    });
+}
+
+function setRandom(h, m, a) {
+    var k = randomKey();
+    var v = randomValue();
+    h.set(k, v);
+    if (!m.hasOwnProperty(k)) {
+        a.push(k);
+    }
+    m[k] = v;
+}
+
+function delRandom(h, m, a) {
+    if (!a.length) {
+        return;
+    }
+    var i = Math.floor(Math.random() * a.length);
+    var k = a[i];
+    h.del(k);
+    delete m[k];
+    a.splice(i, 1);
+}
+
+function randomKey() {
+    return (Math.PI * Math.random()).toString(36).substr(2, 3);
+}
+
+function randomValue() {
+    return Math.floor(Math.random() * 1e9);
+}
+
+basicTests();
+randomTests();
 
 console.log('All tests OK.');
