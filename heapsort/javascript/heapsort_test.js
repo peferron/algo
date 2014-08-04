cbec50015f6496f217d74270c6982648e936d3d4
peferron <pe.ferron@gmail.com>
2014-08-04 18:07:41 +0800

    [javascript] Add random tests to sorting algorithms

--- heapsort/javascript/heapsort_test.js
+++ heapsort/javascript/heapsort_test.js
@@ -5,26 +5,26 @@ var assert = require('assert');
 var heapsort = require('./heapsort.js');
 var maxheap = require('./maxheap.js');
 
-var tests = [
-    {
-        in: null,
-        out: null
-    },
-    {
-        in: [],
-        out: []
-    },
-    {
-        in: [1],
-        out: [1]
-    },
-    {
-        in: [3, 7, 2, 1, 5, 4, 6, 7],
-        out: [1, 2, 3, 4, 5, 6, 7, 7]
-    }
-];
+function basicTests(heapify) {
+    var tests = [
+        {
+            in: null,
+            out: null
+        },
+        {
+            in: [],
+            out: []
+        },
+        {
+            in: [1],
+            out: [1]
+        },
+        {
+            in: [3, 7, 2, 1, 5, 4, 6, 7],
+            out: [1, 2, 3, 4, 5, 6, 7, 7]
+        }
+    ];
 
-function test(heapify) {
     tests.forEach(function(test, i) {
         // console.log('Starting test #' + i + ' with input array', test.in);
 
@@ -34,6 +34,42 @@ function test(heapify) {
     });
 }
 
+function randomTests(heapify) {
+    for (var i = 0; i < 1000; i++) {
+        randomTest(heapify);
+    }
+}
+
+function randomTest(heapify) {
+    var input = randomInts();
+    var a = clone(input);
+
+    heapsort.sort(a, heapify);
+    assert(isSorted(a));
+}
+
+function randomInts() {
+    var n = Math.floor(Math.random() * 1000);
+    var a = [];
+    for (var i = 0; i < n; i++) {
+        // Use n as the upper bound to produce some duplicates.
+        a.push(Math.floor(Math.random() * n));
+    }
+    return a;
+}
+
+function isSorted(a) {
+    if (!a) {
+        return true;
+    }
+    for (var i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function clone(a) {
     if (!a) {
         return a;
@@ -41,7 +77,9 @@ function clone(a) {
     return a.slice(0);
 }
 
-test(maxheap.slowHeapify);
-test(maxheap.fastHeapify);
+basicTests(maxheap.slowHeapify);
+basicTests(maxheap.fastHeapify);
+randomTests(maxheap.slowHeapify);
+randomTests(maxheap.fastHeapify);
 
 console.log('All tests OK.');
--- selection_sort/javascript/selection_sort_test.js
+++ selection_sort/javascript/selection_sort_test.js
@@ -4,24 +4,70 @@ var assert = require('assert');
 
 var selection_sort = require('./selection_sort.js');
 
-var tests = [
-    {
-        in: null,
-        out: null
-    },
-    {
-        in: [],
-        out: []
-    },
-    {
-        in: [1],
-        out: [1]
-    },
-    {
-        in: [3, 7, 2, 1, 5, 4, 6, 7],
-        out: [1, 2, 3, 4, 5, 6, 7, 7]
+function basicTests() {
+    var tests = [
+        {
+            in: null,
+            out: null
+        },
+        {
+            in: [],
+            out: []
+        },
+        {
+            in: [1],
+            out: [1]
+        },
+        {
+            in: [3, 7, 2, 1, 5, 4, 6, 7],
+            out: [1, 2, 3, 4, 5, 6, 7, 7]
+        }
+    ];
+
+    tests.forEach(function(test, i) {
+        // console.log('Starting test #' + i + ' with input array', test.in);
+
+        var a = clone(test.in);
+        selection_sort.sort(a);
+        assert.deepEqual(a, test.out);
+    });
+}
+
+function randomTests() {
+    for (var i = 0; i < 1000; i++) {
+        randomTest();
     }
-];
+}
+
+function randomTest() {
+    var input = randomInts();
+    var a = clone(input);
+
+    selection_sort.sort(a);
+    assert(isSorted(a));
+}
+
+function randomInts() {
+    var n = Math.floor(Math.random() * 1000);
+    var a = [];
+    for (var i = 0; i < n; i++) {
+        // Use n as the upper bound to produce some duplicates.
+        a.push(Math.floor(Math.random() * n));
+    }
+    return a;
+}
+
+function isSorted(a) {
+    if (!a) {
+        return true;
+    }
+    for (var i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function clone(a) {
     if (!a) {
@@ -30,12 +76,7 @@ function clone(a) {
     return a.slice(0);
 }
 
-tests.forEach(function(test, i) {
-    // console.log('Starting test #' + i + ' with input array', test.in);
-
-    var a = clone(test.in);
-    selection_sort.sort(a);
-    assert.deepEqual(a, test.out);
-});
+basicTests();
+randomTests();
 
 console.log('All tests OK.');
