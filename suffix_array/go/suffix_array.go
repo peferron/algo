874da0b8a090b831ce2f3a6101cc24214916db00
peferron <pe.ferron@gmail.com>
2014-08-11 00:44:00 -0700

    [suffix_array/go] First commit

---
+++ suffix_array/go/suffix_array.go
@@ -0,0 +1,103 @@
+package suffix_array
+
+import "sort"
+
+func LongestPalindrome(s string) int {
+	return -1
+}
+
+func FirstOccurrence(s, sub string) int {
+	a := newSuffixArray(s)
+	suffixIndex := searchLow(a, s, sub)
+	start := a[suffixIndex]
+	length := start + len(sub)
+	if length <= len(s) && s[start:length] == sub {
+		return start
+	}
+	return -1
+}
+
+func AllOccurrences(s, sub string) []int {
+	o := []int{}
+	a := newSuffixArray(s)
+	lowSuffixIndex := searchLow(a, s, sub)
+	highSuffixIndex := searchHigh(a, s, sub)
+	for i := lowSuffixIndex; i <= highSuffixIndex; i++ {
+		start := a[i]
+		length := start + len(sub)
+		if length <= len(s) && s[start:length] == sub {
+			o = append(o, start)
+		}
+	}
+	return o
+}
+
+type suffixArray []int
+
+func newSuffixArray(s string) suffixArray {
+	a := suffixArray{}
+	suffixes := []string{}
+	indexes := map[string]int{}
+	for i := 0; i <= len(s); i++ {
+		suffix := s[i:]
+		suffixes = append(suffixes, suffix)
+		indexes[suffix] = i
+	}
+	sort.Strings(suffixes)
+	for _, suffix := range suffixes {
+		index := indexes[suffix]
+		a = append(a, index)
+	}
+	return a
+}
+
+func searchLow(a suffixArray, s, sub string) int {
+	low := 0
+	high := len(a) - 1
+	for low < high {
+		// low <= mid < high
+		mid := (high + low) / 2
+		suffixStart := a[mid]
+		l := suffixStart + len(sub)
+		if l > len(s) {
+			l = len(s)
+		}
+		suffix := s[suffixStart:l]
+		if sub > suffix {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	return low
+}
+
+func searchHigh(a suffixArray, s, sub string) int {
+	low := 0
+	high := len(a) - 1
+	for low < high {
+		// low < mid <= high
+		mid := (high + low + 1) / 2
+		suffixStart := a[mid]
+		l := suffixStart + len(sub)
+		if l > len(s) {
+			l = len(s)
+		}
+		suffix := s[suffixStart:l]
+		if sub < suffix {
+			high = mid - 1
+		} else {
+			low = mid
+		}
+	}
+	return low
+}
+
+// func log(a suffixArray, s string) {
+// 	fmt.Println("[")
+// 	for i, suffixStart := range a {
+// 		suffix := s[suffixStart:]
+// 		fmt.Printf("  %d %d %q\n", i, suffixStart, suffix)
+// 	}
+// 	fmt.Println("]")
+// }
---
+++ suffix_array/go/suffix_array_test.go
@@ -0,0 +1,38 @@
+package suffix_array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLongestPalindrome(t *testing.T) {
+	s := "aba ghhg defabax"
+
+	if i := LongestPalindrome(s); i != 4 {
+		t.Errorf("Expected longest palindrome to return index 4, was %d", i)
+	}
+}
+
+func TestFirstOccurrence(t *testing.T) {
+	s := "aba ghhg defabax"
+
+	if i := FirstOccurrence(s, "bad"); i != -1 {
+		t.Errorf("Expected first occurrence of \"bad\" to be -1, was %d", i)
+	}
+
+	if i := FirstOccurrence(s, "ba"); i != 1 {
+		t.Errorf("Expected first occurrence of \"ba\" to be 1, was %d", i)
+	}
+}
+
+func TestAllOccurrences(t *testing.T) {
+	s := "aba ghhg defabax"
+
+	if a := AllOccurrences(s, "bad"); !reflect.DeepEqual(a, []int{}) {
+		t.Errorf("Expected occurrences of \"ba\" to be [], was %v", a)
+	}
+
+	if a := AllOccurrences(s, "ba"); !reflect.DeepEqual(a, []int{1, 13}) {
+		t.Errorf("Expected occurrences of \"ba\" to be [1, 13], was %v", a)
+	}
+}
