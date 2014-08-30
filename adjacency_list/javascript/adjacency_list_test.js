f7d2fa48a6b5e328cc1d41ecac509a384b9256d7
peferron <pe.ferron@gmail.com>
2014-08-30 18:04:29 +0800

    [adjacency_list/javascript] Add construction test

--- adjacency_list/javascript/adjacency_list_test.js
+++ adjacency_list/javascript/adjacency_list_test.js
@@ -5,6 +5,30 @@ var assert = require('assert');
 var adjacency_list = require('./adjacency_list.js');
 var operations = require('./operations.js');
 
+function testConstruct() {
+    var list = new adjacency_list.construct({
+        vertexCount: 6,
+        edges: [
+            [0, 1],
+            [0, 4],
+            [0, 5],
+            [1, 2],
+            [1, 4],
+            [2, 3],
+            [3, 4]
+        ]
+    });
+
+    assert.deepEqual(list, [
+        [1, 4, 5],
+        [0, 2, 4],
+        [1, 3],
+        [2, 4],
+        [0, 1, 3],
+        [0]
+    ]);
+}
+
 function testBreadthFirstSearch() {
     testSearch(operations.breadthFirstSearch, [0, 1, 4, 5, 2, 3]);
 }
@@ -39,6 +63,7 @@ function testSearch(searchFunction, expectedVertices) {
     assert.deepEqual(vertices, expectedVertices);
 }
 
+testConstruct();
 testBreadthFirstSearch();
 testDepthFirstSearch();
 
