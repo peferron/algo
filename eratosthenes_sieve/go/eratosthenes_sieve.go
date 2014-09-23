2eaab1e8b6b141df4c85900e001086d06de9174e
peferron <pe.ferron@gmail.com>
2014-09-24 00:47:44 +0800

    [eratosthenes_sieve/go] Cleanup

--- eratosthenes_sieve/go/eratosthenes_sieve.go
+++ eratosthenes_sieve/go/eratosthenes_sieve.go
@@ -1,30 +1,42 @@
 package eratosthenes_sieve
 
+// http://stackoverflow.com/a/568618
+
 type Sieve struct {
-	i      int
-	primes []int
+	// The last returned prime.
+	i int
+
+	// Maps each composite to a list of primes that divide it.
+	sieve map[int][]int
 }
 
 func NewSieve() *Sieve {
-	return &Sieve{0, []int{}}
+	return &Sieve{
+		1,
+		map[int][]int{},
+	}
 }
 
 func (s *Sieve) Next() int {
-	s.i++
-	for divisible(s.i, s.primes) {
+	for {
 		s.i++
-	}
-	if s.i != 1 {
-		s.primes = append(s.primes, s.i)
-	}
-	return s.i
-}
 
-func divisible(n int, by []int) bool {
-	for _, b := range by {
-		if n%b == 0 {
-			return true
+		if _, ok := s.sieve[s.i]; !ok {
+			// i is a prime.
+			// All multiples i*j with j < i have already been marked when j has been processed, so
+			// the first multiple that needs to be marked is i*i.
+			s.sieve[s.i*s.i] = []int{s.i}
+			return s.i
 		}
+
+		// i is a composite.
+		for _, p := range s.sieve[s.i] {
+			// i is a multiple of p: i = n*p.
+			// Now, the next multiple of p needs to be marked: m = (n+1)*p = i+p.
+			m := s.i + p
+			s.sieve[m] = append(s.sieve[m], p)
+		}
+		// i won't be needed in the sieve anymore.
+		delete(s.sieve, s.i)
 	}
-	return false
 }
--- eratosthenes_sieve/go/eratosthenes_sieve_test.go
+++ eratosthenes_sieve/go/eratosthenes_sieve_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-var primes = []int{1, 2, 3, 5, 7, 11, 13, 17, 19, 23}
+var primes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47}
 
 func TestNext(t *testing.T) {
 	s := NewSieve()
 	for i, expected := range primes {
 		if actual := s.Next(); actual != expected {
-			t.Errorf("Expected prime number #%d to be %d, was %d", i, expected, actual)
+			t.Errorf("Expected prime #%d to be %d, was %d", i, expected, actual)
 		}
 	}
 }
