e971f9f6e0d7a424ca18e3caa4404825755d2373
peferron <pe.ferron@gmail.com>
2015-05-25 01:09:32 -0700

    [edge_connectivity/rust] Add temporary variable for clarity

--- edge_connectivity/rust/edge_connectivity.rs
+++ edge_connectivity/rust/edge_connectivity.rs
@@ -5,9 +5,10 @@ pub fn edge_connectivity(g: &Graph) -> u32 {
     assert!(!g.directed, "This edge connectivity algorithm only supports undirected graphs");
 
     let maximum_flows = (1..g.vertex_count).map(|i| maximum_flow(g, 0, i));
+    let connectivity = maximum_flows.min();
 
-    match maximum_flows.min() {
-        Some(connectivity) => connectivity,
+    match connectivity {
+        Some(c) => c,
         None => 0,
     }
 }
