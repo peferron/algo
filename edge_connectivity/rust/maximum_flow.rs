4f73d35387cd91ad36d68bbea01625e82681742c
peferron <pe.ferron@gmail.com>
2015-05-25 01:09:04 -0700

    [edge_connectivity/rust] Use optional instead of usize::MAX as a guard value

--- edge_connectivity/rust/maximum_flow.rs
+++ edge_connectivity/rust/maximum_flow.rs
@@ -1,8 +1,6 @@
 use graph::{Edge, Graph};
 use adjacency_matrix::AdjacencyMatrix;
 
-use std::usize;
-
 // maximum_flow returns the maximum flow from source to sink in g.
 pub fn maximum_flow(g: &Graph, source: usize, sink: usize) -> u32 {
     // Initially, the residual flow graph of g is just the same as g.
@@ -22,10 +20,10 @@ impl AdjacencyMatrix {
     // the addition of the augmenting path, and returns the capacity of the augmenting path.
     // If no augmenting path is found, addAugmentingPath returns None.
     fn add_augmenting_path(&mut self, source: usize, sink: usize) -> Option<u32> {
-        let mut parents = vec![usize::MAX; self.size()];
+        let mut parents = vec![None; self.size()];
 
         for Edge { x, y } in self.breadth_first_search(source) {
-            parents[y] = x;
+            parents[y] = Some(x);
             if y == sink {
                 // Abort the BFS.
                 break;
@@ -43,7 +41,7 @@ impl AdjacencyMatrix {
 
     // substractPath substracts the path described by parents and end from the residual flow graph,
     // and returns the capacity of the path.
-    fn substract_path(&mut self, parents: &Vec<usize>, end: usize) -> Option<u32> {
+    fn substract_path(&mut self, parents: &Vec<Option<usize>>, end: usize) -> Option<u32> {
         let capacity = self.path_capacity(parents, end);
 
         if let Some(c) = capacity {
@@ -58,7 +56,7 @@ impl AdjacencyMatrix {
 
     // pathCapacity returns the capacity of the path described by parents and end in the residual
     // flow graph.
-    fn path_capacity(&self, parents: &Vec<usize>, end: usize) -> Option<u32> {
+    fn path_capacity(&self, parents: &Vec<Option<usize>>, end: usize) -> Option<u32> {
         let edges = edges(parents, end);
         let capacities = edges.iter().map(|edge| self[edge.x][edge.y]);
         capacities.min()
@@ -66,13 +64,12 @@ impl AdjacencyMatrix {
 }
 
 // edges returns the edges of the path described by parents and end.
-fn edges(parents: &Vec<usize>, mut end: usize) -> Vec<Edge> {
+fn edges(parents: &Vec<Option<usize>>, mut end: usize) -> Vec<Edge> {
     let mut edges = vec![];
 
-    while parents[end] < usize::MAX {
-        let edge = Edge { x: parents[end], y: end };
-        edges.push(edge);
-        end = parents[end];
+    while let Some(parent) = parents[end] {
+        edges.push(Edge { x: parent, y: end });
+        end = parent;
     }
 
     edges.reverse();
