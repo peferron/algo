563dc002b7b985c9bb575ec3219386f97598ae04
Pierre-Emile Ferron <peferron@gmail.com>
2015-11-22 11:18:01 -0800

    [independent_set/rust] Decompose into separate functions

--- independent_set/rust/independent_set.rs
+++ independent_set/rust/independent_set.rs
@@ -6,11 +6,7 @@ use bit_vector::BitVector;
 // maximal_independent_set processes vertices in order and returns the resulting maximal independent
 // set in g.
 pub fn maximal_independent_set(g: &Graph, vertices: &Vec<usize>) -> Vec<usize> {
-    let mut adjacency_list: Vec<_> = repeat(BitVector::new()).take(g.vertex_count).collect();
-    for &Edge { x, y } in &g.edges {
-        adjacency_list[x].set(y);
-        adjacency_list[y].set(x);
-    }
+    let adjacency_list = adjacency_list(g);
 
     let mut maximal_set = BitVector::new();
     for &x in vertices {
@@ -24,17 +20,35 @@ pub fn maximal_independent_set(g: &Graph, vertices: &Vec<usize>) -> Vec<usize> {
     maximal_set.to_vec()
 }
 
+// adjacency_list returns the adjacency list of g.
+fn adjacency_list(g: &Graph) -> Vec<BitVector> {
+    let mut adjacency_list: Vec<_> = repeat(BitVector::new()).take(g.vertex_count).collect();
+    for &Edge { x, y } in &g.edges {
+        adjacency_list[x].set(y);
+        adjacency_list[y].set(x);
+    }
+    adjacency_list
+}
+
+
 // maximum_independent_set returns an independent set that is hopefully the maximum independent set
 // in g.
 pub fn maximum_independent_set(g: &Graph) -> Vec<usize> {
+    let mut vertices: Vec<_> = (0..g.vertex_count).collect();
+
+    // Sort vertices by increasing degree.
+    let degrees = degrees(g);
+    vertices.sort_by(|&x, &y| degrees[x].cmp(&degrees[y]));
+
+    maximal_independent_set(g, &vertices)
+}
+
+// degrees returns the degree of all vertices in g.
+fn degrees(g: &Graph) -> Vec<usize> {
     let mut degrees = vec![0; g.vertex_count];
     for &Edge { x, y } in &g.edges {
         degrees[x] += 1;
         degrees[y] += 1;
     }
-
-    let mut vertices_sorted_by_increasing_degree: Vec<_> = (0..g.vertex_count).collect();
-    vertices_sorted_by_increasing_degree.sort_by(|&x, &y| degrees[x].cmp(&degrees[y]));
-
-    maximal_independent_set(g, &vertices_sorted_by_increasing_degree)
+    degrees
 }
