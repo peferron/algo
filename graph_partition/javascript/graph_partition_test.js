4fbb34c9890dd07f61319fbb9789ea43cfd1f026
Pierre-Emile Ferron <peferron@gmail.com>
2016-03-03 17:34:28 +0100

    [graph_partition/javascript] Uncomment tests

--- graph_partition/javascript/graph_partition_test.js
+++ graph_partition/javascript/graph_partition_test.js
@@ -3,29 +3,29 @@ const assert = require('assert');
 import {bisect} from './graph_partition.js';
 
 const tests = [
-    // {
-    //     graph: {
-    //         vertexCount: 0,
-    //         edges: []
-    //     },
-    //     subsets: []
-    // },
-    // {
-    //     graph: {
-    //         vertexCount: 1,
-    //         edges: []
-    //     },
-    //     subsets: [0]
-    // },
-    // {
-    //     graph: {
-    //         vertexCount: 3,
-    //         edges: [
-    //             [0, 1]
-    //         ]
-    //     },
-    //     subsets: [0, 0, 1]
-    // },
+    {
+        graph: {
+            vertexCount: 0,
+            edges: []
+        },
+        subsets: []
+    },
+    {
+        graph: {
+            vertexCount: 1,
+            edges: []
+        },
+        subsets: [0]
+    },
+    {
+        graph: {
+            vertexCount: 3,
+            edges: [
+                [0, 1]
+            ]
+        },
+        subsets: [0, 0, 1]
+    },
     {
         graph: {
             vertexCount: 3,
@@ -35,27 +35,27 @@ const tests = [
         },
         subsets: [1, 0, 0]
     },
-    // {
-    //     graph: {
-    //         vertexCount: 4,
-    //         edges: [
-    //             [0, 1], [0, 2], [0, 3],
-    //             [1, 2]
-    //         ]
-    //     },
-    //     subsets: [1, 0, 0, 1]
-    // },
-    // {
-    //     graph: {
-    //         vertexCount: 5,
-    //         edges: [
-    //             [0, 3], [0, 4],
-    //             [1, 2], [1, 4],
-    //             [3, 4]
-    //         ]
-    //     },
-    //     subsets: [1, 0, 0, 1, 1]
-    // }
+    {
+        graph: {
+            vertexCount: 4,
+            edges: [
+                [0, 1], [0, 2], [0, 3],
+                [1, 2]
+            ]
+        },
+        subsets: [1, 0, 0, 1]
+    },
+    {
+        graph: {
+            vertexCount: 5,
+            edges: [
+                [0, 3], [0, 4],
+                [1, 2], [1, 4],
+                [3, 4]
+            ]
+        },
+        subsets: [1, 0, 0, 1, 1]
+    }
 ];
 
 function runTest(test) {
