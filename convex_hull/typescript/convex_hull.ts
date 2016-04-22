fd166b6748e93868db7b1006101c8510428face0
Pierre-Emile Ferron <peferron@gmail.com>
2016-04-22 17:31:39 +0200

    [convex_hull/typescript] Update turn direction function

--- convex_hull/typescript/convex_hull.ts
+++ convex_hull/typescript/convex_hull.ts
@@ -6,24 +6,28 @@ export interface Point2D {
 // eql returns whether the points a and b are equal.
 const eql = (a: Point2D, b: Point2D) => a.x === b.x && a.y === b.y;
 
-// next return the next convex hull point, going clockwise from prev.
-const next = (points: Point2D[], prev: Point2D) =>
-    // This could be optimized to use only one pass.
-    points.filter(p => !eql(prev, p)).reduce((best, p) => left(prev, best, p) ? p : best);
+enum Side {
+    Left,
+    Right,
+    On,
+}
 
-// left returns whether point is to the left of the directed line that passes through lineFrom
-// before lineTo.
-const left = (lineFrom: Point2D, lineTo: Point2D, point: Point2D) =>
+// side returns whether point is to the left, the right, or on the directed line that passes through
+// lineFrom before lineTo.
+const side = (lineFrom: Point2D, lineTo: Point2D, point: Point2D) => {
     // The signed area of the triangle (a, b, c) is half the determinant of the following matrix:
     //     [ ax, ay, 1 ]
     //     [ bx, by, 1 ]
     //     [ cx, cy, 1 ]
-    // If the signed area is positive then c lies to the left of the directed line a->b.
-    // If the signed area is negative then c lies to the right of the directed line a->b.
-    // If the signed area is zero then c lies on the line a-b.
+    // If the signed area is > 0 then c lies to the left of the directed line a->b.
+    // If the signed area is < 0 then c lies to the right of the directed line a->b.
+    // If the signed area is = 0 then c lies on the line a-b.
+
     // The determinant can be calculated with https://en.wikipedia.org/wiki/Rule_of_Sarrus
-    (lineFrom.x * lineTo.y + lineTo.x * point.y + point.x * lineFrom.y -
-    point.x * lineTo.y - lineFrom.x * point.y - lineTo.x * lineFrom.y) > 0;
+    const determinant = lineFrom.x * lineTo.y + lineTo.x * point.y + point.x * lineFrom.y -
+        point.x * lineTo.y - lineFrom.x * point.y - lineTo.x * lineFrom.y;
+    return determinant > 0 ? Side.Left : determinant < 0 ? Side.Right : Side.On;
+};
 
 // giftWrap2D returns the convex hull of the points, using the gift wrapping algorithm.
 export function giftWrap2D(points: Point2D[]): Point2D[] {
@@ -36,12 +40,19 @@ export function giftWrap2D(points: Point2D[]): Point2D[] {
     // Start with the left-most point.
     const start = points.reduce((best, p) => p.x < best.x ? p : best);
 
-    for (let p = start; !hull.length || !eql(p, start); p = next(points, p)) {
+    for (let p = start; !hull.length || !eql(p, start); p = giftWrapNext(points, p)) {
         hull.push(p);
     }
 
     return hull;
 }
+
+// giftWrapNext return the next convex hull point, going clockwise from prev.
+const giftWrapNext = (points: Point2D[], prev: Point2D) =>
+    // This could be optimized to use only one pass.
+    points
+        .filter(p => !eql(prev, p))
+        .reduce((best, p) => side(prev, best, p) === Side.Left ? p : best);
 
 // graham2D returns the convex hull of the points, using the Graham scan algorithm.
 export function graham2D(points: Point2D[]): Point2D[] {
@@ -54,8 +65,16 @@ export function graham2D(points: Point2D[]): Point2D[] {
         p.y < best.y || p.y === best.y && p.x < best.x ? p : best);
 
     // Sort the remaining points in increasing order of the angle they and the start point make with
-    // the x-axis.
-    const ordered = points.filter(p => !eql(start, p)).sort((a, b) => left(start, a, b) ? -1 : 1);
+    // the x-axis. This could be optimized to use only one pass.
+    const ordered = points
+        .filter(p => !eql(start, p))
+        .sort((a, b) => {
+            switch (side(start, a, b)) {
+                case Side.Left: return -1;
+                case Side.Right: return 1;
+                default: return 0;
+            }
+        });
 
     // In order to correctly remove the last possible clockwise turns, we need to make a complete
     // cycle.
@@ -80,7 +99,7 @@ export function graham2D(points: Point2D[]): Point2D[] {
 }
 
 function removeClockwiseTurns(hull: Point2D[]) {
-    while (!left(hull[hull.length - 3], hull[hull.length - 2], hull[hull.length - 1])) {
+    while (side(hull[hull.length - 3], hull[hull.length - 2], hull[hull.length - 1]) === Side.Right) {
         hull.splice(hull.length - 2, 1);
     }
 }
