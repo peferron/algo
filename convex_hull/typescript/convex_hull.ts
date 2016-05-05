4def0b10fb9579c8f0ed9397ca408d22474a5bd8
Pierre-Emile Ferron <peferron@gmail.com>
2016-05-05 22:45:28 +0200

    [convex_hull/typescript] Simplify code

--- convex_hull/typescript/convex_hull.ts
+++ convex_hull/typescript/convex_hull.ts
@@ -6,27 +6,27 @@ export interface Point {
 // eql returns whether the points a and b are equal.
 const eql = (a: Point, b: Point) => a.x === b.x && a.y === b.y;
 
-enum Side {
-    Left,
-    Right,
-    On,
+enum Direction {
+    Clockwise,
+    CounterClockwise,
+    Collinear,
 }
 
-// side returns whether point is to the left, the right, or on the directed line that passes through
-// lineFrom before lineTo.
-const side = (lineFrom: Point, lineTo: Point, point: Point) => {
+// direction returns whether a-b-c forms a counter-clockwise turn, a clockwise turn, or are
+// collinear.
+const direction = (a: Point, b: Point, c: Point) => {
     // The signed area of the triangle (a, b, c) is half the determinant of the following matrix:
     //     [ ax, ay, 1 ]
     //     [ bx, by, 1 ]
     //     [ cx, cy, 1 ]
-    // If the signed area is > 0 then c lies to the left of the directed line a->b.
-    // If the signed area is < 0 then c lies to the right of the directed line a->b.
-    // If the signed area is = 0 then c lies on the line a-b.
+    // If the signed area is > 0 then a-b-c is a counter-clockwise turn.
+    // If the signed area is < 0 then a-b-c is a clockwise turn.
+    // If the signed area is = 0 then a-b-c are collinear.
 
     // The determinant can be calculated with https://en.wikipedia.org/wiki/Rule_of_Sarrus
-    const determinant = lineFrom.x * lineTo.y + lineTo.x * point.y + point.x * lineFrom.y -
-        point.x * lineTo.y - lineFrom.x * point.y - lineTo.x * lineFrom.y;
-    return determinant > 0 ? Side.Left : determinant < 0 ? Side.Right : Side.On;
+    const det = a.x * b.y + b.x * c.y + c.x * a.y - c.x * b.y - a.x * c.y - b.x * a.y;
+
+    return det > 0 ? Direction.CounterClockwise : det < 0 ? Direction.Clockwise : Direction.Collinear;
 };
 
 // giftWrap returns the convex hull of the points, using the gift wrapping algorithm.
@@ -52,7 +52,7 @@ const giftWrapNext = (points: Point[], prev: Point) =>
     // This could be optimized to use only one pass.
     points
         .filter(p => !eql(prev, p))
-        .reduce((best, p) => side(prev, best, p) === Side.Left ? p : best);
+        .reduce((best, p) => direction(prev, best, p) === Direction.Clockwise ? best : p);
 
 // graham returns the convex hull of the points, using the Graham scan algorithm.
 export function graham(points: Point[]): Point[] {
@@ -60,18 +60,18 @@ export function graham(points: Point[]): Point[] {
         return points;
     }
 
-    // Start with the bottom-most point. If there are ties, pick the left-most point.
+    // Start with the first point in lexicographical order. We know that this point must be in the
+    // convex hull.
     const start = points.reduce((best, p) =>
-        p.y < best.y || p.y === best.y && p.x < best.x ? p : best);
+        p.x < best.x || p.x === best.x && p.y < best.y ? p : best);
 
-    // Sort the remaining points in increasing order of the angle they and the start point make with
-    // the x-axis. This could be optimized to use only one pass.
+    // Sort the other points by the slope of the line connecting them to start.
     const ordered = points
         .filter(p => !eql(start, p))
         .sort((a, b) => {
-            switch (side(start, a, b)) {
-                case Side.Left: return -1;
-                case Side.Right: return 1;
+            switch (direction(start, a, b)) {
+                case Direction.CounterClockwise: return -1;
+                case Direction.Clockwise: return 1;
                 default: return 0;
             }
         });
@@ -80,27 +80,19 @@ export function graham(points: Point[]): Point[] {
     // cycle.
     ordered.push(start);
 
-    // We could initialize hull as [start, ordered[0]], and then iterate starting from ordered[1].
-    // However, we know for sure that ordered[0] is in the convex hull, because it's the point with
-    // the lowest angle. So there's no point in running the removeClockwiseTurns function against
-    // [start, ordered[0], ordered[1]].
-    // This means we can initialize hull as [start, ordered[0], ordered[1]] and then iterate
-    // starting from ordered[2]. However, if we do that, at the end of the iteration start will be
-    // present twice, as both the first and last element. We could add a hull.pop(), but it's
-    // simpler to initialize hull to [ordered[0], ordered[1]] and keep start as the last element.
-    const hull: Point[] = [ordered[0], ordered[1]];
+    const hull: Point[] = [];
 
-    for (let i = 2; i < ordered.length; i++) {
-        hull.push(ordered[i]);
-        removeClockwiseTurns(hull);
-    }
+    ordered.forEach(point => {
+        hull.push(point);
+        removeClockwiseAndCollinearTurns(hull);
+    });
 
     return hull;
 }
 
-function removeClockwiseTurns(hull: Point[]) {
+function removeClockwiseAndCollinearTurns(hull: Point[]) {
     while (hull.length > 2 &&
-        side(hull[hull.length - 3], hull[hull.length - 2], hull[hull.length - 1]) !== Side.Left) {
+        direction(hull[hull.length - 3], hull[hull.length - 2], hull[hull.length - 1]) !== Direction.CounterClockwise) {
         hull.splice(hull.length - 2, 1);
     }
 }
@@ -119,13 +111,13 @@ export function monotoneChain(points: Point[]): Point[] {
     const lower = [ordered[0], ordered[1]];
     for (let i = 2; i < n; i++) {
         lower.push(ordered[i]);
-        removeClockwiseTurns(lower);
+        removeClockwiseAndCollinearTurns(lower);
     }
 
     const upper = [ordered[n - 1], ordered[n - 2]];
     for (let i = n - 3; i >= 0; i--) {
         upper.push(ordered[i]);
-        removeClockwiseTurns(upper);
+        removeClockwiseAndCollinearTurns(upper);
     }
 
     // The first and last points in ordered are present in both lower and upper. We need to make
