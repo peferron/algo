b3e884b20e6c09e9daf4d7854515283526459a88
Pierre-Emile Ferron <peferron@gmail.com>
2016-05-09 23:22:51 +0800

    [EPI/5.11] First commit

---
+++ EPI/5.11/swift/5.11.swift
@@ -0,0 +1,43 @@
+// swiftlint:disable variable_name
+
+// Rectangles are usually describes with words such as 'top', 'bottom' and so on, but it can be
+// confusing: math typically points the y-axis upwards, while computer graphics point it downwards.
+// Therefore, we define rectangles as an origin point and a size; the origin point plus the size
+// gives the diagonal point.
+
+public typealias Point = (x: Int, y: Int)
+public typealias Size = (w: Int, h: Int)
+
+public struct Rect {
+    let origin: Point
+    let size: Size
+
+    var diagonal: Point {
+        return (x: origin.x + size.w, y: origin.y + size.h)
+    }
+
+    init(origin: Point, size: Size) {
+        self.origin = origin
+        self.size = size
+    }
+
+    init(origin: Point, diagonal: Point) {
+        self.origin = origin
+        self.size = (w: diagonal.x - origin.x, h: diagonal.y - origin.y)
+    }
+}
+
+public func intersection(a: Rect, _ b: Rect) -> Rect? {
+    let intersection = Rect(
+        origin: (
+            x: max(a.origin.x, b.origin.x),
+            y: max(a.origin.y, b.origin.y)
+        ),
+        diagonal: (
+            x: min(a.diagonal.x, b.diagonal.x),
+            y: min(a.diagonal.y, b.diagonal.y)
+        )
+    )
+
+    return intersection.size.w >= 0 && intersection.size.h >= 0 ? intersection : nil
+}
---
+++ EPI/5.11/swift/5.11_test.swift
@@ -0,0 +1,39 @@
+// swiftlint:disable variable_name
+
+import Darwin
+
+func == (lhs: Rect, rhs: Rect) -> Bool {
+    return lhs.origin == rhs.origin && lhs.size == rhs.size
+}
+
+let tests: [(a: Rect, b: Rect, intersection: Rect?)] = [
+    (
+        Rect(origin: (0, 0), size: (5, 5)),
+        Rect(origin: (1, 1), size: (2, 3)),
+        Rect(origin: (1, 1), size: (2, 3))
+    ),
+    (
+        Rect(origin: (0, 0), size: (5, 5)),
+        Rect(origin: (-5, -5), size: (7, 8)),
+        Rect(origin: (0, 0), size: (2, 3))
+    ),
+    (
+        Rect(origin: (0, 0), size: (5, 5)),
+        Rect(origin: (5, 4), size: (1, 7)),
+        Rect(origin: (5, 4), size: (0, 1))
+    ),
+    (
+        Rect(origin: (0, 0), size: (5, 5)),
+        Rect(origin: (0, 6), size: (5, 5)),
+        nil
+    ),
+]
+
+for test in tests {
+    let actual = intersection(test.a, test.b)
+    guard actual == nil ? test.intersection == nil : actual! == test.intersection! else {
+        print("For rectangles \(test.a) and \(test.b), " +
+            "expected intersection to be \(test.intersection), but was \(actual)")
+        exit(1)
+    }
+}
