17698ea906caba569b3b11b564a10fdb943a9ca8
Pierre-Emile Ferron <peferron@gmail.com>
2016-05-12 22:39:44 +0800

    [EPI/6.15] First commit

---
+++ EPI/6.15/swift/6.15.swift
@@ -0,0 +1,33 @@
+// swiftlint:disable variable_name
+
+func swap(inout array: [Int], _ i: Int, _ j: Int) {
+    (array[i], array[j]) = (array[j], array[i])
+}
+
+// O(n) time and space, since we build an array of size n.
+public func randomSubsetArray(n n: Int, k: Int) -> [Int] {
+    var array = (0..<n).map { $0 }
+
+    for i in 0..<k {
+        let r = i + Int(arc4random_uniform(UInt32(n - i)))
+        swap(&array, i, r)
+    }
+
+    return [Int](array.prefix(k))
+}
+
+// Works exactly like randomSubsetArray, but with a dictionary to record value swaps instead of an
+// array. Reduces complexity to O(k) time and space.
+public func randomSubsetDictionary(n n: Int, k: Int) -> [Int] {
+    var values = [Int: Int]()
+
+    for i in 0..<k {
+        let r = i + Int(arc4random_uniform(UInt32(n - i)))
+        let vi = values[i] ?? i
+        let vr = values[r] ?? r
+        values[i] = vr
+        values[r] = vi
+    }
+
+    return (0..<k).map { values[$0]! }
+}
---
+++ EPI/6.15/swift/6.15_test.swift
@@ -0,0 +1,41 @@
+// swiftlint:disable variable_name
+
+import Darwin
+
+let fns = [randomSubsetArray, randomSubsetDictionary]
+
+func test(fn: (n: Int, k: Int) -> [Int] ) {
+    let n = Int(arc4random_uniform(100))
+    let k = Int(arc4random_uniform(UInt32(n) + 1))
+    let subset = fn(n: n, k: k)
+
+    guard isValidSubset(subset, n: n, k: k) else {
+        print("For n \(n) and k \(k), got invalid subset \(subset)")
+        exit(1)
+    }
+}
+
+func isValidSubset(subset: [Int], n: Int, k: Int) -> Bool {
+    // Verify that subset has the expected count.
+    guard subset.count == k else {
+        return false
+    }
+
+    // Verify that subset has no duplicates.
+    guard Set(subset).count == k else {
+        return false
+    }
+
+    // Verify that the elements of subset are in 0..<n.
+    guard !subset.contains({ $0 < 0 && $0 >= n }) else {
+        return false
+    }
+
+    return true
+}
+
+for fn in fns {
+    for _ in 0...100 {
+        test(fn)
+    }
+}
