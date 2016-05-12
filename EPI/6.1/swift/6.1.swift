38899e22047f8a8c8410957a005fcceb57712a4f
Pierre-Emile Ferron <peferron@gmail.com>
2016-05-12 22:01:04 +0800

    [EPI/6.1] Don't use enumerate while mutating the enumerated array

--- EPI/6.1/swift/6.1.swift
+++ EPI/6.1/swift/6.1.swift
@@ -4,20 +4,21 @@ public func dutchInPlace(inout array: [Int], pivotIndex: Int) {
     let pivot = array[pivotIndex]
     var lowerCount = 0
     var equalCount = 0
-    for (index, element) in array.enumerate() {
-        if element < pivot {
-            // Append element to the lower section. We do that by swapping element with the first
+
+    for i in 0..<array.count {
+        if array[i] < pivot {
+            // Append array[i] to the lower section. We do that by swapping array[i] with the first
             // element equal or greater than the pivot.
-            swap(&array, index, lowerCount)
+            swap(&array, i, lowerCount)
             if equalCount > 0 {
-                // array[index] now contains an element equal to the pivot. To keep respecting the
-                // Dutch layout, we need to move it to the end of the equal section.
-                swap(&array, index, lowerCount + equalCount)
+                // array[i] now contains an element equal to the pivot. To keep respecting the Dutch
+                // layout, we need to move it to the end of the equal section.
+                swap(&array, i, lowerCount + equalCount)
             }
             lowerCount += 1
-        } else if element == pivot {
-            // Append element to the equal section.
-            swap(&array, index, lowerCount + equalCount)
+        } else if array[i] == pivot {
+            // Append array[i] to the equal section.
+            swap(&array, i, lowerCount + equalCount)
             equalCount += 1
         }
     }
