9299087f30c9c4e8a623d57f18696dd652ab7f64
Pierre-Emile Ferron <peferron@gmail.com>
2016-05-13 19:09:14 +0200

    [EPI/7.4] Split into sub-functions

--- EPI/7.4/swift/7.4.swift
+++ EPI/7.4/swift/7.4.swift
@@ -1,8 +1,19 @@
 // "a" becomes "dd", "b" is removed.
 public func replaceAndRemove(inout characters: [Character], count: Int) -> Int {
     // The first pass removes "b" and counts "a".
+    let (aCount, bCount) = removeB(&characters, count: count)
+
+    // After the first pass, there are (count - bCount) characters that still matter.
+    // The second pass goes backwards from there, replacing "a" with "dd".
+    replaceA(&characters, count: count - bCount, aCount: aCount)
+
+    return count - bCount + aCount
+}
+
+func removeB(inout characters: [Character], count: Int) -> (Int, Int) {
     var aCount = 0
     var bCount = 0
+
     for i in 0..<count {
         let character = characters[i]
         if character == "b" {
@@ -15,20 +26,20 @@ public func replaceAndRemove(inout characters: [Character], count: Int) -> Int {
         }
     }
 
-    let finalCount = count - bCount + aCount
+    return (aCount, bCount)
+}
 
-    // After the first pass, there are (count - bCount) characters that still matter.
-    // The second pass goes backwards from there, replacing "a" with "dd".
-    for i in (0..<count - bCount).reverse() {
+func replaceA(inout characters: [Character], count: Int, aCount: Int) {
+    var remainingACount = aCount
+
+    for i in (0..<count).reverse() {
         let character = characters[i]
         if character == "a" {
-            characters[i + aCount] = "d"
-            characters[i + aCount - 1] = "d"
-            aCount -= 1
+            characters[i + remainingACount] = "d"
+            characters[i + remainingACount - 1] = "d"
+            remainingACount -= 1
         } else {
-            characters[i + aCount] = character
+            characters[i + remainingACount] = character
         }
     }
-
-    return finalCount
 }
