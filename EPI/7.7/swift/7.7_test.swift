80ced372d4b7087a7a1d671818b54c5a0c4c3bd3
Pierre-Emile Ferron <peferron@gmail.com>
2016-05-14 05:36:10 +0200

    [EPI/7.7] Add tests

--- EPI/7.7/swift/7.7_test.swift
+++ EPI/7.7/swift/7.7_test.swift
@@ -3,6 +3,9 @@ import Darwin
 let fns = [mnemonicsIterative, mnemonicsRecursive]
 
 let tests: [(number: String, mnemonics: [String])] = [
+    ("", [""]),
+    ("0", ["0"]),
+    ("4", ["G", "H", "I"]),
     ("137", ["1DP", "1DQ", "1DR", "1DS", "1EP", "1EQ", "1ER", "1ES", "1FP", "1FQ", "1FR", "1FS"])
 ]
 
