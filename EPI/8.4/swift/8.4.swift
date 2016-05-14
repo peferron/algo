c5342d7bc8fb622c79c1ae85edd9f702da81630a
Pierre-Emile Ferron <peferron@gmail.com>
2016-05-14 13:37:46 +0200

    [EPI/8.4] First commit

---
+++ EPI/8.4/swift/8.4.swift
@@ -0,0 +1,62 @@
+// swiftlint:disable variable_name
+
+public class Node {
+    var next: Node?
+}
+
+// cycleStart returns the node at the start of the cycle, or nil if there is no cycle.
+public func cycleStart(list: Node) -> Node? {
+    if let node = cycleAny(list) {
+        let count = cycleCount(node)
+        return cycleStart(list, cycleCount: count)
+    }
+    return nil
+}
+
+// cycleAny returns any node in the cycle, or nil if there is no cycle.
+func cycleAny(list: Node) -> Node? {
+    var slow = list
+    var fast = list.next
+
+    while let f = fast, fnn = f.next?.next {
+        if slow === f {
+            return slow
+        }
+        slow = slow.next!
+        fast = fnn
+    }
+
+    return nil
+}
+
+// cycleCount returns the number of nodes in the cycle containing node.
+func cycleCount(node: Node) -> Int {
+    var next = node.next!
+    var count = 1
+
+    while next !== node {
+        count += 1
+        next = next.next!
+    }
+
+    return count
+}
+
+// cycleStart returns the first node of the cycle containing cycleCount nodes.
+func cycleStart(list: Node, cycleCount: Int) -> Node {
+    // Use two cursors separated by cycleCount nodes. They will point to the same node after the
+    // second cursor completes a full cycle.
+    var firstCursor = list
+    var secondCursor = list
+
+    for _ in 0..<cycleCount {
+        secondCursor = secondCursor.next!
+    }
+
+    while firstCursor !== secondCursor {
+        firstCursor = firstCursor.next!
+        secondCursor = secondCursor.next!
+    }
+
+    return firstCursor
+}
---
+++ EPI/8.4/swift/8.4_test.swift
@@ -0,0 +1,36 @@
+// swiftlint:disable variable_name
+
+import Darwin
+
+func toNodes(nexts: [Int?]) -> [Node] {
+    let nodes = (0..<nexts.count).map { _ in Node() }
+    for (i, next) in nexts.enumerate() where next != nil {
+        nodes[i].next = nodes[next!]
+    }
+    return nodes
+}
+
+let tests: [(nexts: [Int?], cycleStartIndex: Int?)] = [
+    ([nil], nil),
+    ([1, 2, nil], nil),
+    ([0], 0),
+    ([1, 0], 0),
+    ([1, 2, 1], 1),
+    ([1, 2, 3, 4, 5, 6, 0], 0),
+    ([1, 2, 3, 4, 5, 6, 2], 2),
+    ([1, 2, 3, 4, 5, 6, 4], 4),
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6], 6),
+]
+
+for test in tests {
+    let nodes = toNodes(test.nexts)
+    let actualCycleStart = cycleStart(nodes.first!)
+    let actual = actualCycleStart == nil ? nil : nodes.indexOf { $0 === actualCycleStart! }
+
+    guard actual == nil && test.cycleStartIndex == nil ||
+        actual != nil && test.cycleStartIndex != nil && actual! == test.cycleStartIndex! else {
+        print("For nexts \(test.nexts), expected cycleStartIndex to be \(test.cycleStartIndex), " +
+            "but was \(actual)")
+        exit(1)
+    }
+}
