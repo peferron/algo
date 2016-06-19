8b92b75297e7d53a949f81b86f80e8f05eadffe0
Pierre-Emile Ferron <peferron@gmail.com>
2016-06-18 18:31:51 -0700

    [EPI/11.1] Rename MinHeap to Heap

--- EPI/11.1/swift/11.1.swift
+++ EPI/11.1/swift/11.1.swift
@@ -1,11 +1,11 @@
 // swiftlint:disable variable_name
 
-class MinHeap<T> {
-    let lower: (T, T) -> Bool
+class Heap<T> {
+    let higherPriority: (T, than: T) -> Bool
     private var array = [T]()
 
-    init(lower: (T, T) -> Bool) {
-        self.lower = lower
+    init(higherPriority: (T, than: T) -> Bool) {
+        self.higherPriority = higherPriority
     }
 
     private func parentIndex(i: Int) -> Int? {
@@ -17,7 +17,7 @@ class MinHeap<T> {
     }
 
     private func bubbleUp(i: Int) {
-        if let pi = parentIndex(i) where lower(array[i], array[pi]) {
+        if let pi = parentIndex(i) where higherPriority(array[i], than: array[pi]) {
             swap(i, pi)
             bubbleUp(pi)
         }
@@ -40,21 +40,21 @@ class MinHeap<T> {
 
     private func bubbleDown(i: Int) {
         if let lci = leftChildIndex(i) {
-            let minChildIndex: Int
-            if let rci = rightChildIndex(i) where lower(array[rci], array[lci]) {
-                minChildIndex = rci
+            let priorityChildIndex: Int
+            if let rci = rightChildIndex(i) where higherPriority(array[rci], than: array[lci]) {
+                priorityChildIndex = rci
             } else {
-                minChildIndex = lci
+                priorityChildIndex = lci
             }
 
-            if lower(array[minChildIndex], array[i]) {
-                swap(minChildIndex, i)
-                bubbleDown(minChildIndex)
+            if higherPriority(array[priorityChildIndex], than: array[i]) {
+                swap(priorityChildIndex, i)
+                bubbleDown(priorityChildIndex)
             }
         }
     }
 
-    func removeMin() -> T? {
+    func removeHighestPriority() -> T? {
         switch array.count {
         case 0:
             return nil
@@ -77,7 +77,7 @@ public func merge<T: Comparable>(sequences: [[T]]) -> [T] {
     let currentElement = { sequences[$0][currentIndexes[$0]] }
 
     // First, we insert the first element of each sequence into the heap.
-    let minHeap = MinHeap { currentElement($0) < currentElement($1) }
+    let minHeap = Heap { currentElement($0) < currentElement($1) }
     for (sequenceIndex, sequence) in sequences.enumerate() where !sequence.isEmpty {
         minHeap.insert(sequenceIndex)
     }
@@ -85,7 +85,7 @@ public func merge<T: Comparable>(sequences: [[T]]) -> [T] {
     // Then, we repeatedly remove the minimum element from the heap, and insert the next element (if
     // any) from the corresponding sequence.
     var merged = [T]()
-    while let sequenceIndex = minHeap.removeMin() {
+    while let sequenceIndex = minHeap.removeHighestPriority() {
         merged.append(currentElement(sequenceIndex))
         currentIndexes[sequenceIndex] += 1
         if currentIndexes[sequenceIndex] < sequences[sequenceIndex].count {
