61a63b8eb82f91f729ced44c61b671084729331e
Pierre-Emile Ferron <peferron@gmail.com>
2016-12-28 16:00:52 -0800

    [EPI/15.1] Add comment about possible improvements

--- EPI/15.1/swift/15.1.swift
+++ EPI/15.1/swift/15.1.swift
@@ -15,6 +15,12 @@ extension Node {
         var result = true
         var prev = Int.min
 
+        // An obvious improvement here would be to abort the DFS as soon as `result` turns false.
+        // One solution would be to return a Bool in the closure, instead of void: `true` would
+        // continue the DFS, and `false` would abort it.
+        // A much more idiomatic solution would be to change the `depthFirstSearch` function to
+        // return an iterator (lazy sequence) instead. Then we could just run a plain `for` loop
+        // over this iterator, and `break` out of it when the BST test fails.
         depthFirstSearch { node in
             result = result && node.value >= prev
             prev = node.value
