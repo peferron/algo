f0520329011cf8fd3595c479d6e3595b05c18845
Pierre-Emile Ferron <peferron@gmail.com>
2017-04-10 10:01:32 -0700

    [EPI/14.3] Add alternate implementation

--- EPI/14.3/swift/14.3.swift
+++ EPI/14.3/swift/14.3.swift
@@ -14,3 +14,21 @@ public func occurrences(_ string: String) -> [Occurrences] {
 
     return result
 }
+
+// The book says that sorting the string directly doesn't use additional space, but it does, since
+// it allocates a new array of chars, which is linear with string length. Using dictionary, on the
+// other hand, only takes space linear with the number of distinct chars, which is bounded by some
+// constant (although this constant will be very high for e.g. Chinese). Also, in Swift, iterating
+// the string characters is lazy and does not allocate a new array.
+/*
+public func occurrences(_ string: String) -> [(Character, Int)] {
+    var counts = [Character: Int]()
+
+    for char in string.characters {
+        let count = counts[char] ?? 0
+        counts[char] = count + 1
+    }
+
+    return counts.sorted { $0.0 < $1.0 }
+}
+*/
