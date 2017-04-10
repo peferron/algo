6b01796738b1f5dae9b8aee708d17897903ed515
Pierre-Emile Ferron <peferron@gmail.com>
2017-04-09 20:58:56 -0700

    [EPI/17.5] Use set instead of dict

--- EPI/17.5/swift/17.5.swift
+++ EPI/17.5/swift/17.5.swift
@@ -5,44 +5,38 @@ public func contains(grid: [[Int]], sequence: [Int]) -> Bool {
         return true
     }
 
-    // results[i][j][k] is true if there exists a path starting at cell (i, j) that contains the
-    // sequence suffix of length k.
-    var results = grid.map { row in
-        row.map { _ in [Int: Bool]() }
+    // impossible[i][j] contains k if there is no path starting at (i, j) that retraces the sequence
+    // suffix of length k.
+    var impossible = grid.map { row in
+        row.map { _ in Set<Int>() }
     }
 
     return grid.enumerated().contains { (i, row) in
         row.enumerated().contains { (j, _) in
-            contains(grid: grid, sequence: ArraySlice(sequence), start: (i, j), results: &results)
+            contains(grid: grid, sequence: ArraySlice(sequence), start: (i, j), impossible: &impossible)
         }
     }
 }
 
-private func contains(grid: [[Int]], sequence: ArraySlice<Int>, start: Cell, results: inout [[[Int: Bool]]]) -> Bool {
-    // Check if the result is cached.
-    if let result = results[start.i][start.j][sequence.count] {
-        return result
+private func contains(grid: [[Int]], sequence: ArraySlice<Int>, start: Cell, impossible: inout [[Set<Int>]]) -> Bool {
+    if impossible[start.i][start.j].contains(sequence.count) {
+        return false
     }
 
-    let result: Bool
+    if grid[start.i][start.j] == sequence.first! {
+        // The first element matches, but what about the rest?
+        let remaining = sequence.dropFirst()
 
-    if grid[start.i][start.j] != sequence.first! {
-        // We cannot even match the first element.
-        result = false
-    } else if sequence.count == 1 {
-        // We matched the entire sequence!
-        result = true
-    } else {
-        // We matched the first element, but can we also match the next elements?
-        result = neighbors(grid: grid, cell: start).contains { neighbor in
-            contains(grid: grid, sequence: sequence.dropFirst(), start: neighbor, results: &results)
+        if remaining.isEmpty || neighbors(grid: grid, cell: start).contains { neighbor in
+            contains(grid: grid, sequence: remaining, start: neighbor, impossible: &impossible)
+        } {
+            return true
         }
     }
 
-    // Cache the result.
-    results[start.i][start.j][sequence.count] = result
-
-    return result
+    // Cache this failure.
+    impossible[start.i][start.j].insert(sequence.count)
+    return false
 }
 
 private func neighbors(grid: [[Int]], cell: Cell) -> [Cell] {
@@ -52,6 +46,7 @@ private func neighbors(grid: [[Int]], cell: Cell) -> [Cell] {
         (cell.i, cell.j - 1),
         (cell.i, cell.j + 1),
     ].filter { neighbor in
-        neighbor.i >= 0 && neighbor.i < grid.count && neighbor.j >= 0 && neighbor.j < grid[0].count
+        neighbor.i >= 0 && neighbor.i < grid.count &&
+        neighbor.j >= 0 && neighbor.j < grid[neighbor.i].count
     }
 }
