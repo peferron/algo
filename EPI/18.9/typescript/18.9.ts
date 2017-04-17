9eb113bb315aab894a9de4245ac882ff0b087410
Pierre-Emile Ferron <peferron@gmail.com>
2017-04-16 18:07:25 -0700

    [EPI/18.9/typescript] First commit

---
+++ EPI/18.9/typescript/18.9.ts
@@ -0,0 +1,18 @@
+export default function maxArea(heights: number[]): number {
+    let maxArea = 0;
+    const stack = [{index: -1, height: 0}];
+
+    [...heights, 0].forEach((h, i) => {
+        while (stack.length > 1 && h <= stack[stack.length - 1].height) {
+            const {height} = stack.pop()!;
+            const right = i - 1;
+            const left = stack[stack.length - 1].index + 1;
+            const area = (right - left + 1) * height;
+            maxArea = Math.max(maxArea, area);
+        }
+
+        stack.push({index: i, height: h});
+    });
+
+    return maxArea;
+}
