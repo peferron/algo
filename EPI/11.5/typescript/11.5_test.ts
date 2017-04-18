3edb4c3f7a8b5db7055a5323ed2a992ec6ee639b
Pierre-Emile Ferron <peferron@gmail.com>
2017-04-17 23:35:06 -0700

    [EPI/11.5/typescript] Uncomment tests

--- EPI/11.5/typescript/11.5_test.ts
+++ EPI/11.5/typescript/11.5_test.ts
@@ -5,14 +5,14 @@ const assert = require('assert');
 const inspect = (v: any) => require('util').inspect(v, {depth: null});
 
 const tests: {values: number[], medians: number[]}[] = [
-    // {
-    //     values: [],
-    //     medians: []
-    // },
-    // {
-    //     values: [1],
-    //     medians: [1]
-    // },
+    {
+        values: [],
+        medians: []
+    },
+    {
+        values: [1],
+        medians: [1]
+    },
     {
         values: [1, 0, 3, 5, 2, 0, 1],
         medians: [1, 0.5, 1, 2, 2, 1.5, 1]
