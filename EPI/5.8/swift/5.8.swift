8c7bf73df394013f68c3f55e65edbd3d4966f3fe
Pierre-Emile Ferron <peferron@gmail.com>
2017-04-17 23:55:40 -0700

    [EPI/5.8/swift] Convert to new Swift version

--- EPI/5.8/swift/5.8.swift
+++ EPI/5.8/swift/5.8.swift
@@ -1,5 +1,5 @@
 // O(n) where n is the number of digits in base 10 of the provided number.
-public func reverse(number: Int) -> Int {
+public func reverse(_ number: Int) -> Int {
     var n = abs(number)
     var reversed = 0
 
