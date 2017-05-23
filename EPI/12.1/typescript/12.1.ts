23d9ac8575a46f1f2579d15b368ff361a04355af
Pierre-Emile Ferron <peferron@gmail.com>
2017-05-22 23:07:22 -0700

    [EPI/12.1/typescript] First commit

---
+++ EPI/12.1/typescript/12.1.ts
@@ -0,0 +1,16 @@
+export default function firestIndexOf<T>(array: T[], element: T): number {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (right >= left) {
+        const mid = Math.floor(left - (right - left) / 2);
+        if (array[mid] >= element) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    const index = right + 1;
+    return index >= 0 && index < array.length && array[index] === element ? index : -1;
+}
