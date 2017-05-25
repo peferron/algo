2e1dc2e444343c33719151cf982711dbef5ac82b
Pierre-Emile Ferron <peferron@gmail.com>
2017-05-25 02:11:30 -0700

    [EPI/13.8/swift] Convert to new Swift version

--- EPI/13.8/swift/13.8.swift
+++ EPI/13.8/swift/13.8.swift
@@ -1,32 +1,33 @@
 public func digest(article: [String], search: Set<String>) -> Range<Int>? {
     var bestRange: Range<Int>?
-    var currentRange = 0..<0
+    var currentRangeStart = 0
+    var currentRangeEnd = 0
     var currentSearchCounts = [String: Int]()
 
     while true {
         if currentSearchCounts.count < search.count {
             // The current range is missing some search words. Increment the end index to try to
             // include all search words.
-            if currentRange.endIndex == article.count {
+            if currentRangeEnd == article.count {
                 break
             }
-            let incomingWord = article[currentRange.endIndex]
-            currentRange.endIndex += 1
+            let incomingWord = article[currentRangeEnd]
+            currentRangeEnd += 1
             if search.contains(incomingWord) {
                 currentSearchCounts[incomingWord] = (currentSearchCounts[incomingWord] ?? 0) + 1
             }
         } else {
             // The current range contains all search words. Increment the start index to try to
             // reduce the range count.
-            if bestRange == nil || currentRange.count < bestRange!.count {
-                bestRange = currentRange
+            if bestRange == nil || currentRangeEnd - currentRangeStart < bestRange!.count {
+                bestRange = Range(currentRangeStart..<currentRangeEnd)
             }
-            let outgoingWord = article[currentRange.startIndex]
-            currentRange.startIndex += 1
-            if let count = currentSearchCounts[outgoingWord] where count > 1 {
+            let outgoingWord = article[currentRangeStart]
+            currentRangeStart += 1
+            if let count = currentSearchCounts[outgoingWord], count > 1 {
                 currentSearchCounts[outgoingWord] = count - 1
             } else {
-                currentSearchCounts.removeValueForKey(outgoingWord)
+                currentSearchCounts.removeValue(forKey: outgoingWord)
             }
         }
     }
--- EPI/13.8/swift/13.8_test.swift
+++ EPI/13.8/swift/13.8_test.swift
@@ -14,30 +14,30 @@ let tests: [(article: [String], search: Set<String>, digest: Range<Int>?)] = [
     (
         article: ["abc"],
         search: ["abc"],
-        digest: 0...0
+        digest: 0..<1
     ),
     (
         article: ["abc", "def", "ghi", "jkl", "mno"],
         search: ["abc"],
-        digest: 0...0
+        digest: 0..<1
     ),
     (
         article: ["abc", "def", "ghi", "jkl", "mno"],
         search: ["jkl", "def"],
-        digest: 1...3
+        digest: 1..<4
     ),
     (
         article: ["abc", "def", "ghi", "jkl", "mno", "def", "jkl"],
         search: ["jkl", "def"],
-        digest: 5...6
+        digest: 5..<7
     ),
 ]
 
 for test in tests {
-    let actual = digest(test.article, search: test.search)
+    let actual = digest(article: test.article, search: test.search)
     guard actual == test.digest else {
         print("For test article \(test.article) and search \(test.search), " +
-            "expected range of digest to be \(test.digest), but was \(actual)")
+            "expected range of digest to be \(test.digest!), but was \(actual!)")
         exit(1)
     }
 }
