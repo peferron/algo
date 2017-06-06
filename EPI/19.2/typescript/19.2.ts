bff235cf7afd699eee15f613c0cbbb6b1928f2ff
Pierre-Emile Ferron <peferron@gmail.com>
2017-06-06 01:14:10 -0700

    [EPI/19.2/typescript] First commit

---
+++ EPI/19.2/typescript/19.2.ts
@@ -0,0 +1,27 @@
+export interface Cell {
+    row: number;
+    col: number;
+}
+
+export function flip(grid: boolean[][], cell: Cell): void {
+    const color = grid[cell.row][cell.col];
+    grid[cell.row][cell.col] = !color;
+
+    for (const neighbor of neighbors(grid, cell)) {
+        if (grid[neighbor.row][neighbor.col] === color) {
+            flip(grid, neighbor);
+        }
+    }
+}
+
+function neighbors(grid: boolean[][], {row, col}: Cell): Cell[] {
+    return [
+        {row: row - 1, col},
+        {row, col: col + 1},
+        {row: row + 1, col},
+        {row, col: col - 1},
+    ].filter(({row: r, col: c}) =>
+        r >= 0 && r < grid.length &&
+        c >= 0 && c < grid[row].length
+    );
+}
