0851331e6044781e1a6e02468b72fc8fd036b4e0
Pierre-Emile Ferron <peferron@gmail.com>
2017-12-17 23:13:38 -0800

    [EPI/12.12/swift] Convert to new Swift version

--- EPI/12.12/swift/12.12.swift
+++ EPI/12.12/swift/12.12.swift
@@ -1,15 +1,15 @@
-public func findDuplicateAndMissing(numbers: [Int]) -> (duplicate: Int, missing: Int) {
+public func findDuplicateAndMissing(_ numbers: [Int]) -> (duplicate: Int, missing: Int) {
     let range = 0..<numbers.count
 
-    let numbersXor = numbers.reduce(0, combine: ^)
-    let rangeXor = range.reduce(0, combine: ^)
+    let numbersXor = numbers.reduce(0, ^)
+    let rangeXor = range.reduce(0, ^)
 
     let duplicateAndMissingXor = numbersXor ^ rangeXor
 
     let differingBit = duplicateAndMissingXor & ~(duplicateAndMissingXor - 1)
 
-    let filteredNumbersXor = numbers.lazy.filter { $0 & differingBit != 0 }.reduce(0, combine: ^)
-    let filteredRangeXor = range.lazy.filter { $0 & differingBit != 0 }.reduce(0, combine: ^)
+    let filteredNumbersXor = numbers.lazy.filter { $0 & differingBit != 0 }.reduce(0, ^)
+    let filteredRangeXor = range.lazy.filter { $0 & differingBit != 0 }.reduce(0, ^)
 
     let duplicateOrMissing = filteredNumbersXor ^ filteredRangeXor
     let other = duplicateOrMissing ^ duplicateAndMissingXor
