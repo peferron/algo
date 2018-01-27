2eb1cc8216d443d63f99d6a59c951bb38cc8bbcb
Pierre-Emile Ferron <peferron@gmail.com>
2018-01-26 18:25:23 -0800

    [EPI/6.2/swift] Convert to new Swift version

--- EPI/6.2/swift/6.2.swift
+++ EPI/6.2/swift/6.2.swift
@@ -1,10 +1,10 @@
-public func increment(inout number: [Int]) {
-    for i in (0..<number.count).reverse() {
+public func increment(_ number: inout [Int]) {
+    for i in (0..<number.count).reversed() {
         if number[i] < 9 {
             number[i] += 1
             return
         }
         number[i] = 0
     }
-    number.insert(1, atIndex: 0)
+    number.insert(1, at: 0)
 }
