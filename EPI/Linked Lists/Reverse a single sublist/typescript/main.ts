04e5404e47cc80bbf12bb4bd9fc77d8f2e5002db
Pierre-Emile Ferron <peferron@gmail.com>
2018-12-15 21:39:50 -0800

    [EPI/Lists/ReverseSublist/typescript] Add alternative implementation

--- EPI/Linked Lists/Reverse a single sublist/typescript/main.ts
+++ EPI/Linked Lists/Reverse a single sublist/typescript/main.ts
@@ -2,7 +2,8 @@ export interface Node {
     next?: this;
 }
 
-export function reverse(head: Node, start: number, end: number): Node {
+// Reverses the nodes from `start` to `end` inclusive (1-indexed).
+export function reverseRange(head: Node, start: number, end: number): Node {
     const tmpHead: Node = {next: head};
     let beforeStartNode = tmpHead;
 
@@ -21,4 +22,34 @@ export function reverse(head: Node, start: number, end: number): Node {
     }
 
     return tmpHead.next!;
+}
+
+// Reverses the nodes from `start` to `end` inclusive (1-indexed).
+// Alternative implementation.
+export function reverseRange2(head: Node, start: number, end: number): Node {
+    const tmpHead: Node = {next: head};
+    let beforeStartNode = tmpHead;
+
+    for (let i = 1; i < start; i += 1) {
+        beforeStartNode = beforeStartNode.next!;
+    }
+
+    beforeStartNode.next = reverseFirst(beforeStartNode.next!, end - start + 1);
+    return tmpHead.next!;
+}
+
+// Reverses the first `count` nodes.
+function reverseFirst(head: Node, count: number): Node {
+    let prev: Node | undefined;
+    let curr: Node | undefined = head;
+
+    for (let i = 0; i < count; i += 1) {
+        const next: Node | undefined = curr!.next;
+        curr!.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    head.next = curr;
+    return prev!;
 }
--- EPI/Linked Lists/Reverse a single sublist/typescript/test.ts
+++ EPI/Linked Lists/Reverse a single sublist/typescript/test.ts
@@ -1,4 +1,4 @@
-import {Node, reverse} from './main';
+import {Node, reverseRange, reverseRange2} from './main';
 
 declare function require(name: string): any;
 const assert = require('assert');
@@ -13,53 +13,55 @@ function make(length: number): Node[] {
     return nodes;
 }
 
-{
-    const [n1, n2, n3] = make(3);
-    const reversed = reverse(n1, 1, 2);
-    assert.strictEqual(reversed, n2);
-    assert.strictEqual(reversed.next, n1);
-    assert.strictEqual(reversed.next!.next, n3);
-}
+for (const fn of [reverseRange, reverseRange2]) {
+    {
+        const [n1, n2, n3] = make(3);
+        const reversed = fn(n1, 1, 2);
+        assert.strictEqual(reversed, n2);
+        assert.strictEqual(reversed.next, n1);
+        assert.strictEqual(reversed.next!.next, n3);
+    }
 
-{
-    const [n1, n2, n3] = make(3);
-    const reversed = reverse(n1, 2, 3);
-    assert.strictEqual(reversed, n1);
-    assert.strictEqual(reversed.next, n3);
-    assert.strictEqual(reversed.next!.next, n2);
-}
+    {
+        const [n1, n2, n3] = make(3);
+        const reversed = fn(n1, 2, 3);
+        assert.strictEqual(reversed, n1);
+        assert.strictEqual(reversed.next, n3);
+        assert.strictEqual(reversed.next!.next, n2);
+    }
 
-{
-    const [n1, n2, n3] = make(3);
-    const reversed = reverse(n1, 1, 3);
-    assert.strictEqual(reversed, n3);
-    assert.strictEqual(reversed.next, n2);
-    assert.strictEqual(reversed.next!.next, n1);
-}
+    {
+        const [n1, n2, n3] = make(3);
+        const reversed = fn(n1, 1, 3);
+        assert.strictEqual(reversed, n3);
+        assert.strictEqual(reversed.next, n2);
+        assert.strictEqual(reversed.next!.next, n1);
+    }
 
-{
-    const [n1, n2, n3] = make(3);
-    const reversed = reverse(n1, 2, 2);
-    assert.strictEqual(reversed, n1);
-    assert.strictEqual(reversed.next, n2);
-    assert.strictEqual(reversed.next!.next, n3);
-}
+    {
+        const [n1, n2, n3] = make(3);
+        const reversed = fn(n1, 2, 2);
+        assert.strictEqual(reversed, n1);
+        assert.strictEqual(reversed.next, n2);
+        assert.strictEqual(reversed.next!.next, n3);
+    }
 
-{
-    const [n1, n2, n3, n4] = make(4);
-    const reversed = reverse(n1, 2, 3);
-    assert.strictEqual(reversed, n1);
-    assert.strictEqual(reversed.next, n3);
-    assert.strictEqual(reversed.next!.next, n2);
-    assert.strictEqual(reversed.next!.next!.next, n4);
-}
+    {
+        const [n1, n2, n3, n4] = make(4);
+        const reversed = fn(n1, 2, 3);
+        assert.strictEqual(reversed, n1);
+        assert.strictEqual(reversed.next, n3);
+        assert.strictEqual(reversed.next!.next, n2);
+        assert.strictEqual(reversed.next!.next!.next, n4);
+    }
 
-{
-    const [n1, n2, n3, n4, n5] = make(5);
-    const reversed = reverse(n1, 2, 4);
-    assert.strictEqual(reversed, n1);
-    assert.strictEqual(reversed.next, n4);
-    assert.strictEqual(reversed.next!.next, n3);
-    assert.strictEqual(reversed.next!.next!.next, n2);
-    assert.strictEqual(reversed.next!.next!.next!.next, n5);
+    {
+        const [n1, n2, n3, n4, n5] = make(5);
+        const reversed = fn(n1, 2, 4);
+        assert.strictEqual(reversed, n1);
+        assert.strictEqual(reversed.next, n4);
+        assert.strictEqual(reversed.next!.next, n3);
+        assert.strictEqual(reversed.next!.next!.next, n2);
+        assert.strictEqual(reversed.next!.next!.next!.next, n5);
+    }
 }
