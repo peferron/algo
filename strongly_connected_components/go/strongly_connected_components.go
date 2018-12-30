31f698bd76e3538dea22b58f248f57859c513bac
Pierre-Emile Ferron <peferron@gmail.com>
2018-12-30 09:46:56 +0100

    [strongly_connected_components] Use stack

--- strongly_connected_components/go/strongly_connected_components.go
+++ strongly_connected_components/go/strongly_connected_components.go
@@ -1,72 +1,65 @@
 package strongly_connected_components
 
 func StronglyConnectedComponents(g Graph) []int {
-	labels := label(NewAdjacencyList(g))
-	t := NewAdjacencyList(transpose(g))
-
-	// Perform depth-first searches starting from the highest-labelled vertex in t, until all vertices have been
-	// processed. Each resulting DFS tree is a strongly connected component.
-	components := make([]int, len(t))
-	currentComponent := 0
-	assignComponent := func(x int) {
-		labels[x] = -1
-		components[x] = currentComponent
-	}
-
-	// Reuse the discovered array to avoid processing the same vertices multiple times.
-	// We could get rid of this array completely, and check labels[x] >= 0 instead, but the code is simpler this way.
-	discovered := make([]bool, len(t))
-
-	for {
-		max := indexOfMax(labels)
-		if labels[max] < 0 {
-			return components
-		}
-		t.DepthFirstSearch(max, assignComponent, discovered)
-		currentComponent++
-	}
+	dfsCompletionOrder := traverse(g)
+	return label(g, dfsCompletionOrder)
 }
 
-// label performs depth-first searches starting from arbitrary vertices of l until all vertices have been processed,
-// labelling each vertex in order of its completion (not discovery).
-func label(l AdjacencyList) []int {
-	labels := make([]int, len(l))
-	for i := range labels {
-		labels[i] = -1
-	}
-
-	currentLabel := 0
-	labelVertex := func(x int) {
-		labels[x] = currentLabel
-		currentLabel++
+// traverse performs depth-first searches starting from arbitrary vertices of g until all vertices have been processed,
+// returning a list of vertices in order of their completion (not discovery).
+func traverse(g Graph) []int {
+	l := NewAdjacencyList(g)
+	stack := make([]int, 0, len(l))
+	push := func(y int) {
+		stack = append(stack, y)
 	}
 
 	// Reuse the discovered array to avoid processing the same vertices multiple times.
 	discovered := make([]bool, len(l))
 
-	for i := range l {
-		l.DepthFirstSearch(i, labelVertex, discovered)
+	for x := range l {
+		l.DepthFirstSearch(x, push, discovered)
 	}
 
-	return labels
+	return stack
+}
+
+// label returns the connected component number of each vertex in g, using a previously generated DFS completion order.
+func label(g Graph, dfsCompletionOrder []int) []int {
+	t := NewAdjacencyList(transpose(g))
+
+	components := make([]int, len(t))
+	for x := range components {
+		components[x] = -1
+	}
+	currentComponent := 0
+	assignComponent := func(x int) {
+		components[x] = currentComponent
+	}
+
+	// Reuse the discovered array to avoid processing the same vertices multiple times.
+	// We could get rid of this array completely, and check components[x] >= 0 instead, but the code is simpler this
+	// way.
+	discovered := make([]bool, len(t))
+
+	for i := len(dfsCompletionOrder) - 1; i >= 0; i-- {
+		x := dfsCompletionOrder[i]
+		if components[x] < 0 {
+			t.DepthFirstSearch(x, assignComponent, discovered)
+			currentComponent++
+		}
+	}
+
+	return components
 }
 
 // transpose returns a copy of g, with the direction of each edge reversed.
 func transpose(g Graph) Graph {
 	t := make([]Edge, len(g.Edges))
+
 	for i, e := range g.Edges {
 		t[i] = Edge{e.Y, e.X}
 	}
-	return Graph{g.VertexCount, g.Directed, t}
-}
 
-// indexOfMax returns the index of the highest element in a, or 0 if a is empty.
-func indexOfMax(a []int) int {
-	m := 0
-	for i, v := range a {
-		if v > a[m] {
-			m = i
-		}
-	}
-	return m
+	return Graph{g.VertexCount, g.Directed, t}
 }
