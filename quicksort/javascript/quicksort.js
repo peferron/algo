3b0be64e147089ffda57fb3b1f421cba9f345807
Pierre-Emile Ferron <peferron@gmail.com>
2019-02-16 20:19:09 -0800

    [quicksort/javascript] Use exclusive range end

--- quicksort/javascript/quicksort.js
+++ quicksort/javascript/quicksort.js
@@ -1,60 +1,60 @@
 export default function sort(a) {
-    if (!a) {
-        return;
+    if (a) {
+        sortRec(a, 0, a.length);
     }
-    sortRec(a, 0, a.length - 1);
 }
 
+// `start` is inclusive and `end` is exclusive.
 function sortRec(a, start, end) {
-    if (end < start + 1) {
+    if (end - start < 2) {
         return;
     }
-    const p = partition(a, start, end);
-    sortRec(a, start, p - 1);
-    sortRec(a, p + 1, end);
+
+    const originalPivotIndex = medianPivotIndex(a, start, end);
+    const finalPivotIndex = partition(a, start, end, originalPivotIndex);
+    sortRec(a, start, finalPivotIndex);
+    sortRec(a, finalPivotIndex + 1, end);
 }
 
-function partition(a, start, end) {
-    const p = medianPivot(a, start, end);
-    swap(a, p, end);
-    let firstHigh = start;
+function partition(a, start, end, pivotIndex) {
+    const pivotValue = a[pivotIndex];
+    swap(a, pivotIndex, end - 1);
+    let lowerCount = 0;
 
     for (let i = start; i < end; i += 1) {
-        if (a[i] < a[end]) {
-            swap(a, firstHigh, i);
-            firstHigh += 1;
+        if (a[i] <= pivotValue) {
+            swap(a, start + lowerCount, i);
+            lowerCount += 1;
         }
     }
 
-    swap(a, firstHigh, end);
-    return firstHigh;
+    return start + lowerCount - 1;
+}
+
+function medianPivotIndex(a, start, end) {
+    const mid = start + Math.floor((end - start) / 2);
+
+    if (between(a[start], a[mid], a[end - 1])) {
+        return start;
+    }
+    if (between(a[mid], a[start], a[end - 1])) {
+        return mid;
+    }
+    return end - 1;
 }
 
 function swap(a, i, j) {
-    const t = a[i];
+    const tmp = a[i];
     a[i] = a[j];
-    a[j] = t;
+    a[j] = tmp;
 }
 
-function medianPivot(a, start, end) {
-    const mid = start + Math.floor((end - start) / 2);
-
-    if (between(a[start], a[mid], a[end])) {
-        return start;
-    }
-    if (between(a[mid], a[start], a[end])) {
-        return mid;
-    }
-    return end;
+function between(value, bound1, bound2) {
+    return bound1 <= value && value <= bound2 || bound2 <= value && value <= bound1;
 }
 
-function between(a, x, y) {
-    return x <= a && a <= y || y <= a && a <= x;
-}
-
-/* randomPivot is an alternative to medianPivot, but medianPivot is recommended.
-function randomPivot(a, start, end) {
-    // Math.random() returns a number in the range [0, 1)
-    return start + Math.floor(Math.random() * (end - start + 1));
+/* randomPivotIndex is an alternative to medianPivotIndex, but medianPivotIndex is recommended.
+function randomPivotIndex(start, end) {
+    return start + Math.floor(Math.random() * (end - start));
 }
 */
