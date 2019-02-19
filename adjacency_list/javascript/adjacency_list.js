09a7a127cdea38c9ffcac5b28754628a4803fb72
Pierre-Emile Ferron <peferron@gmail.com>
2019-02-18 23:21:14 -0800

    [adjacency_list/javascript] Process edges

--- adjacency_list/javascript/adjacency_list.js
+++ adjacency_list/javascript/adjacency_list.js
@@ -1,15 +1,36 @@
-export default class AdjacencyList {
+// Vertex states
+const UNDISCOVERED = 'undiscovered';
+const DISCOVERED = 'discovered';
+const PROCESSED = 'processed';
+
+// Edge types
+export const TREE = 'tree';
+export const BACK = 'back';
+export const FORWARD = 'forward';
+export const CROSS = 'cross';
+
+export class AdjacencyList {
     constructor(graph) {
         this.a = constructAdjacencyList(graph);
+        this.directed = graph.directed;
     }
 
-    breadthFirstSearch(start, callback) {
-        breadthFirstSearch(this.a, start, callback);
+    breadthFirstSearch(start, vertexCallback, edgeCallback) {
+        breadthFirstSearch(this.a, this.directed, start, vertexCallback, edgeCallback);
     }
 
-    depthFirstSearch(start, preOrderCallback, postOrderCallback) {
-        const discovered = new Array(this.a.length);
-        depthFirstSearch(this.a, start, preOrderCallback, postOrderCallback, discovered);
+    depthFirstSearch(start, preOrderCallback, postOrderCallback, edgeCallback) {
+        const metadata = Array.from(this.a, () => ({state: UNDISCOVERED}));
+
+        // Directed vs. undirected does not change how DFS processes vertices, but it changes how DFS processes and
+        // classifies edges. Handling both cases within the same DFS function makes that function less readable,
+        // especially compared to an undirected-only implementation. To keep each case as readable as possible, each is
+        // implemented as a separate function.
+        if (this.directed) {
+            depthFirstSearchDirected(this.a, start, preOrderCallback, postOrderCallback, edgeCallback, metadata, 0);
+        } else {
+            depthFirstSearchUndirected(this.a, start, preOrderCallback, postOrderCallback, edgeCallback, metadata);
+        }
     }
 }
 
@@ -32,12 +53,12 @@ function insertEdge(a, x, y, directed) {
     }
 }
 
-function breadthFirstSearch(a, start, callback) {
-    const discovered = new Array(a.length);
+function breadthFirstSearch(a, directed, start, vertexCallback, edgeCallback) {
+    const states = new Array(a.length).fill(UNDISCOVERED);
     const queue = [];
 
-    discovered[start] = true;
-    callback(start);
+    states[start] = DISCOVERED;
+    vertexCallback(start);
     queue.push(start);
 
     while (queue.length) {
@@ -45,27 +66,80 @@ function breadthFirstSearch(a, start, callback) {
         const x = queue.shift();
 
         for (const y of a[x]) {
-            if (discovered[y]) {
-                continue;
+            if (states[y] !== PROCESSED || directed) {
+                edgeCallback([x, y]);
             }
-            discovered[y] = true;
-            callback(y);
-            queue.push(y);
+
+            if (states[y] === UNDISCOVERED) {
+                states[y] = DISCOVERED;
+                vertexCallback(y);
+                queue.push(y);
+            }
         }
+
+        states[x] = PROCESSED;
     }
 }
 
-function depthFirstSearch(a, start, preOrderCallback, postOrderCallback, discovered) {
-    if (discovered[start]) {
+function depthFirstSearchUndirected(a, x, preOrderCallback, postOrderCallback, edgeCallback, metadata) {
+    if (metadata[x].state !== UNDISCOVERED) {
         return;
     }
 
-    discovered[start] = true;
-    preOrderCallback(start);
+    metadata[x].state = DISCOVERED;
+    preOrderCallback(x);
 
-    for (const neighbor of a[start]) {
-        depthFirstSearch(a, neighbor, preOrderCallback, postOrderCallback, discovered);
+    for (const y of a[x]) {
+        let edgeType;
+
+        if (metadata[y].state === UNDISCOVERED) {
+            edgeType = TREE;
+            metadata[y].parent = x;
+        } else if (metadata[y].state === DISCOVERED && metadata[x].parent !== y) {
+            edgeType = BACK;
+        } else {
+            // This edge has already been explored.
+        }
+
+        if (edgeType) {
+            edgeCallback([x, y, edgeType]);
+        }
+
+        depthFirstSearchUndirected(a, y, preOrderCallback, postOrderCallback, edgeCallback, metadata);
     }
 
-    postOrderCallback(start);
+    metadata[x].state = PROCESSED;
+    postOrderCallback(x);
+}
+
+function depthFirstSearchDirected(a, x, preOrderCallback, postOrderCallback, edgeCallback, metadata, time) {
+    if (metadata[x].state !== UNDISCOVERED) {
+        return time;
+    }
+
+    metadata[x].state = DISCOVERED;
+    metadata[x].entryTime = time;
+    time += 1;
+
+    preOrderCallback(x);
+
+    for (const y of a[x]) {
+        let edgeType;
+
+        if (metadata[y].state === UNDISCOVERED) {
+            edgeType = TREE;
+            metadata[y].parent = x;
+        } else if (metadata[y].state === DISCOVERED) {
+            edgeType = BACK;
+        } else if (metadata[y].state === PROCESSED) {
+            edgeType = metadata[y].entryTime < metadata[x].entryTime ? CROSS : FORWARD;
+        }
+
+        edgeCallback([x, y, edgeType]);
+        time = depthFirstSearchDirected(a, y, preOrderCallback, postOrderCallback, edgeCallback, metadata, time);
+    }
+
+    metadata[x].state = PROCESSED;
+    postOrderCallback(x);
+    return time;
 }
--- adjacency_list/javascript/adjacency_list_test.js
+++ adjacency_list/javascript/adjacency_list_test.js
@@ -1,48 +1,144 @@
 import assert from 'assert';
-import AdjacencyList from './adjacency_list';
+import {AdjacencyList, TREE, BACK, FORWARD, CROSS} from './adjacency_list';
 
-const graph = {
-    vertexCount: 6,
-    directed: false,
-    edges: [
-        [0, 1],
-        [0, 4],
-        [0, 5],
-        [1, 2],
-        [1, 4],
-        [2, 3],
-        [3, 4],
-    ],
-};
+const tests = [
+    {
+        graph: {
+            vertexCount: 6,
+            directed: false,
+            edges: [
+                [0, 1],
+                [0, 4],
+                [0, 5],
+                [1, 2],
+                [1, 4],
+                [2, 3],
+                [3, 4],
+            ],
+        },
+        list: [
+            [1, 4, 5],
+            [0, 2, 4],
+            [1, 3],
+            [2, 4],
+            [0, 1, 3],
+            [0],
+        ],
+        bfs: {
+            vertices: [0, 1, 4, 5, 2, 3],
+            edges: [
+                [0, 1],
+                [0, 4],
+                [0, 5],
+                [1, 2],
+                [1, 4],
+                [4, 3],
+                [2, 3],
+            ],
+        },
+        dfs: {
+            verticesPreOrder: [0, 1, 2, 3, 4, 5],
+            verticesPostOrder: [4, 3, 2, 1, 5, 0],
+            edges: [
+                [0, 1, TREE],
+                [1, 2, TREE],
+                [2, 3, TREE],
+                [3, 4, TREE],
+                [4, 0, BACK],
+                [4, 1, BACK],
+                [0, 5, TREE],
+            ],
+        },
+    },
+    {
+        graph: {
+            vertexCount: 8,
+            directed: true,
+            edges: [
+                [0, 1],
+                [1, 2],
+                [1, 3],
+                [1, 4],
+                [2, 0],
+                [3, 0],
+                [3, 5],
+                [3, 7],
+                [4, 5],
+                [5, 6],
+                [6, 4],
+                [7, 5],
+            ],
+        },
+        list: [
+            [1],
+            [2, 3, 4],
+            [0],
+            [0, 5, 7],
+            [5],
+            [6],
+            [4],
+            [5],
+        ],
+        bfs: {
+            vertices: [0, 1, 2, 3, 4, 5, 7, 6],
+            edges: [
+                [0, 1],
+                [1, 2],
+                [1, 3],
+                [1, 4],
+                [2, 0],
+                [3, 0],
+                [3, 5],
+                [3, 7],
+                [4, 5],
+                [5, 6],
+                [7, 5],
+                [6, 4],
+            ],
+        },
+        dfs: {
+            verticesPreOrder: [0, 1, 2, 3, 5, 6, 4, 7],
+            verticesPostOrder: [2, 4, 6, 5, 7, 3, 1, 0],
+            edges: [
+                [0, 1, TREE],
+                [1, 2, TREE],
+                [2, 0, BACK],
+                [1, 3, TREE],
+                [3, 0, BACK],
+                [3, 5, TREE],
+                [5, 6, TREE],
+                [6, 4, TREE],
+                [4, 5, BACK],
+                [3, 7, TREE],
+                [7, 5, CROSS],
+                [1, 4, FORWARD],
+            ],
+        },
+    },
+];
 
-function testConstruct() {
-    const list = new AdjacencyList(graph);
-    assert.deepStrictEqual(list.a, [
-        [1, 4, 5],
-        [0, 2, 4],
-        [1, 3],
-        [2, 4],
-        [0, 1, 3],
-        [0],
-    ]);
+for (const test of tests) {
+    // Test construction.
+    const list = new AdjacencyList(test.graph);
+    assert.deepStrictEqual(list.a, test.list);
+
+    // Test BFS.
+    {
+        const vertices = [];
+        const edges = [];
+        list.breadthFirstSearch(0, x => vertices.push(x), e => edges.push(e));
+        assert.deepStrictEqual(vertices, test.bfs.vertices);
+        assert.deepStrictEqual(edges, test.bfs.edges);
+    }
+
+    // Test DFS.
+    {
+        const verticesPreOrder = [];
+        const verticesPostOrder = [];
+        const edges = [];
+        list.depthFirstSearch(0, x => verticesPreOrder.push(x), x => verticesPostOrder.push(x), e => edges.push(e));
+        assert.deepStrictEqual(verticesPreOrder, test.dfs.verticesPreOrder);
+        assert.deepStrictEqual(verticesPostOrder, test.dfs.verticesPostOrder);
+        assert.deepStrictEqual(edges, test.dfs.edges);
+    }
 }
-
-function testBreadthFirstSearch() {
-    const list = new AdjacencyList(graph);
-    const order = [];
-    list.breadthFirstSearch(0, x => order.push(x));
-    assert.deepStrictEqual(order, [0, 1, 4, 5, 2, 3]);
-}
-
-function testDepthFirstSearch() {
-    const list = new AdjacencyList(graph);
-    const preOrder = [];
-    const postOrder = [];
-    list.depthFirstSearch(0, x => preOrder.push(x), x => postOrder.push(x));
-    assert.deepStrictEqual(preOrder, [0, 1, 2, 3, 4, 5]);
-    assert.deepStrictEqual(postOrder, [4, 3, 2, 1, 5, 0]);
-}
-
-testConstruct();
-testBreadthFirstSearch();
-testDepthFirstSearch();
