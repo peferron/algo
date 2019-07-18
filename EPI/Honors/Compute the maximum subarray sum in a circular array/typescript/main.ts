0a95a149b991e2ab340ff227b3701d1e258ddcfe
Pierre-Emile Ferron <peferron@gmail.com>
2019-07-17 22:50:35 -0700

    [EPI/Honors/Subarray/typescript] Comment out unimplemented module

--- EPI/Honors/Compute the maximum subarray sum in a circular array/typescript/main.ts
+++ EPI/Honors/Compute the maximum subarray sum in a circular array/typescript/main.ts
@@ -1,17 +1,17 @@
-export function maxSubarraySumBruteForce(a: number[]): number {
-    const n = a.length;
-    let max = 0;
+// export function maxSubarraySumBruteForce(a: number[]): number {
+//     const n = a.length;
+//     let max = 0;
 
-    for (let start = 0; start < n; start += 1) {
-        let sum = 0;
-        for (let i = 0; i < n; i += 1) {
-            sum += a[(start + i) % n];
-            max = Math.max(max, sum);
-        }
-    }
+//     for (let start = 0; start < n; start += 1) {
+//         let sum = 0;
+//         for (let i = 0; i < n; i += 1) {
+//             sum += a[(start + i) % n];
+//             max = Math.max(max, sum);
+//         }
+//     }
 
-    return max;
-}
+//     return max;
+// }
 
-export function maxSubarraySumSmart(a: number[]): number {
-}
+// export function maxSubarraySumSmart(a: number[]): number {
+// }
